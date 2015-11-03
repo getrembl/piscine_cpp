814745a9c63102f20ae87854fe7962d68f613f62
getrembl <getrembl@student.42.fr>
2015-11-03 23:09:16 +0100

    add D00/ex01

---
+++ ex01/NewContact.class.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+class Newcontact
+{
+public:
+
+  NewContact(); // constructeur
+  string first_name;
+  string last_name;
+  string nickname;
+  string login;
+  string postal_adress;
+  string email_adress;
+  string phone_number;
+string birthday
+  _NewContact(); // destructeur
+};
